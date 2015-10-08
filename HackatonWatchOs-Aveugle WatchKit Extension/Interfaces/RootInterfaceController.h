2589f96fc5ede979996be7f52bb184bffc72a2ed
Fonsat Nicolas <fonsat.nicolas@gmail.com>
2015-10-08 12:05:04 +0200

    Ajout d'un contrôleur root

---
+++ HackatonWatchOs-Aveugle WatchKit Extension/Interfaces/RootInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  RootInterfaceController.h
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Etudiant on 08/10/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface RootInterfaceController : WKInterfaceController
+
+@end
