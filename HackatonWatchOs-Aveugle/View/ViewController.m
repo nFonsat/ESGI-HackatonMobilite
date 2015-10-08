43ff244cd0bce67180b84c9475d42e6993f856d4
Fonsat Nicolas <fonsat.nicolas@gmail.com>
2015-10-08 09:27:02 +0200

    about de la map view et son implémentation dans "ViewController"

--- HackatonWatchOs-Aveugle/View/ViewController.m
+++ HackatonWatchOs-Aveugle/View/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate, CLLocationManagerDelegate> {
+    @private
+    CLLocationManager* locationManager;
+    BOOL locationFound;
+}
 
 @end
 
@@ -16,12 +21,94 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    [self initMapView];
+    
+    [self initLocationManager];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - MapView impl
+
+- (void)initMapView {
+    locationFound = false;
+    self.mapView.showsUserLocation = true;
+    self.mapView.delegate = self;
+}
+
+
+#pragma mark - CoreLocation impl
+
+- (void)initLocationManager {
+    locationManager = [CLLocationManager new];
+    locationManager.delegate = self;
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+}
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation{
+    
+    if (!locationFound) {
+        locationFound = true;
+        
+        MKCoordinateRegion region;
+        MKCoordinateSpan span;
+        
+        span.latitudeDelta = 0.005;
+        span.longitudeDelta = 0.005;
+        
+        CLLocationCoordinate2D location;
+        
+        location.latitude = userLocation.coordinate.latitude;
+        location.longitude = userLocation.coordinate.longitude;
+        
+        region.span = span;
+        region.center = location;
+        
+        [self.mapView setRegion:region animated:YES];
+        
+        [self.mapView setCenterCoordinate:location];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    NSString* statusString;
+    
+    switch (status) {
+        case kCLAuthorizationStatusNotDetermined:
+            statusString = @"User has not yet made a choice with regards to this application";
+            [locationManager requestAlwaysAuthorization];
+            break;
+            
+        case kCLAuthorizationStatusRestricted:
+            statusString = @"This application is not authorized to use location services";
+            
+        case kCLAuthorizationStatusDenied:
+            statusString = @"User has explicitly denied authorization for this application";
+            [locationManager requestAlwaysAuthorization];
+            break;
+            
+        case kCLAuthorizationStatusAuthorizedAlways:
+            statusString = @"User has granted authorization to use their location at any time";
+            [locationManager startUpdatingLocation];
+            break;
+            
+        case kCLAuthorizationStatusAuthorizedWhenInUse:
+            statusString = @"User has granted authorization to use their location only when your app is visible";
+            [locationManager startUpdatingLocation];
+            break;
+            
+        default:
+            statusString = @"Not found status";
+            break;
+    }
+    
+    NSLog(@"Authorization => %@", statusString);
 }
 
 @end
