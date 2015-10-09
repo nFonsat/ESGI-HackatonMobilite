42a8507e2ea43e4085493108df7c20420f6df8b3
Fonsat Nicolas <fonsat.nicolas@gmail.com>
2015-10-09 09:11:54 +0200

    Remplacement de la page GMHomepage par GTestView

--- HackatonWatchOs-Aveugle/Application/GMHomePageController.m
+++ HackatonWatchOs-Aveugle/Application/GMHomePageController.m
@@ -7,9 +7,8 @@
 //
 
 #import "GMHomePageController.h"
-#import <WatchConnectivity/WatchConnectivity.h>
 
-@interface GMHomePageController () <WCSessionDelegate>
+@interface GMHomePageController ()
 
 @end
 
@@ -17,54 +16,12 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [self initWCSession];
     // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-#pragma mark - WCSession impl
-
-- (void) initWCSession {
-    if ([WCSession class] && [WCSession isSupported]) {
-        WCSession* session = [WCSession defaultSession];
-        session.delegate = self;
-        [session activateSession];
-    }
-}
-
-
-#pragma mark - Action User interface impl
-
-- (IBAction)onReadyAction:(UIButton *)sender {
-    NSLog(@"onReadyAction");
-    if ([WCSession defaultSession].reachable) {
-        [[WCSession defaultSession] sendMessage:@{@"isReady" : @"true"}
-                                   replyHandler:nil
-                                   errorHandler:nil];
-    }
-}
-
-- (IBAction)onRoadworksDetected:(id)sender {
-    NSLog(@"onRoadworksDetected");
-    if ([WCSession defaultSession].reachable) {
-        [[WCSession defaultSession] sendMessage:@{@"danger" : @"roadwork"}
-                                   replyHandler:nil
-                                   errorHandler:nil];
-    }
-}
-
-- (IBAction)onTraficLightDetected:(id)sender {
-    NSLog(@"onTraficLightDetected");
-    if ([WCSession defaultSession].reachable) {
-        [[WCSession defaultSession] sendMessage:@{@"danger" : @"trafic_light"}
-                                   replyHandler:nil
-                                   errorHandler:nil];
-    }
 }
 
 @end
---
+++ HackatonWatchOs-Aveugle/Application/GMTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  GMTestViewController.h
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Etudiant on 09/10/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMTestViewController : UIViewController
+
+@end
