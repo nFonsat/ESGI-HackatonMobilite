ebb8da2b7f2a314b0aae4698e16af7b5b7124a7b
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-09 17:46:31 +0100

    remove StoryBoard and add MainView

---
+++ HackatonWatchOs-Aveugle/Views/Main/GMMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  GMMainViewController.h
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 09/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMMainViewController : UIViewController
+
+@end
---
+++ HackatonWatchOs-Aveugle/Views/Main/GMMainViewController.m
@@ -0,0 +1,22 @@
+//
+//  GMMainViewController.m
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 09/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "GMMainViewController.h"
+
+@interface GMMainViewController ()
+
+@end
+
+@implementation GMMainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"Guid'me";
+}
+
+@end
