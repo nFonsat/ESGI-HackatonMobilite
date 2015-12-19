9b8f5fa2f9a7d3dbfbdf1cf9e9c5b34de490ce9d
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-19 03:12:12 +0100

    add location home and map view

---
+++ HackatonWatchOs-Aveugle/Views/Location/Home/GMLocationHomeViewController.m
@@ -0,0 +1,37 @@
+//
+//  GMLocationHomeViewController.m
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 19/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "GMLocationHomeViewController.h"
+
+@interface GMLocationHomeViewController ()
+
+@end
+
+@implementation GMLocationHomeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ HackatonWatchOs-Aveugle/Views/Location/Map/GMLocationMapViewController.m
@@ -0,0 +1,37 @@
+//
+//  GMLocationMapViewController.m
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 19/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "GMLocationMapViewController.h"
+
+@interface GMLocationMapViewController ()
+
+@end
+
+@implementation GMLocationMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
