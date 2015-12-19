fff05d1c68b9cc5f89ec8dbbff3e5bc787764948
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-19 04:29:29 +0100

    impl Location Web API

--- HackatonWatchOs-Aveugle/Web/API/Location/GMWebLocationAPI.m
+++ HackatonWatchOs-Aveugle/Web/API/Location/GMWebLocationAPI.m
@@ -10,4 +10,109 @@
 
 @implementation GMWebLocationAPI
 
+- (instancetype)init
+{
+    return [self initWithBaseURL:@"/api/v1/location"];
+}
+
+- (void)getLocationsSuccess:(void (^)(id responseObject))success
+                     Failure:(void (^)(NSError *error))failure
+{
+    [self authenticateUserWithCredential];
+    [self.OAuth2Manager GET:[NSString stringWithFormat:@"%@/all",self.baseURL]
+                 parameters:nil
+                    success:^(AFHTTPRequestOperation *operation, id responseObject)
+     {
+         success(responseObject);
+     }
+                    failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         failure(error);
+     }];
+}
+
+- (void)postLocationWithName:(NSString *)name
+                     Location:(CLLocation *)location
+                      Success:(void (^)(id responseObject))success
+                      Failure:(void (^)(NSError *error))failure
+{
+    [self authenticateUserWithCredential];
+    
+    NSDictionary * parameters = @{
+                                  @"name":name,
+                                  @"latitude": @(location.coordinate.latitude),
+                                  @"longitude": @(location.coordinate.latitude),
+                                  };
+    
+    
+    [self.OAuth2Manager POST:self.baseURL
+                 parameters:parameters
+                    success:^(AFHTTPRequestOperation *operation, id responseObject)
+     {
+         success(responseObject);
+     }
+                    failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         failure(error);
+     }];
+}
+
+- (void)getLocationWithLocationId:(NSString *)locationId
+                           Success:(void (^)(id responseObject))success
+                           Failure:(void (^)(NSError *error))failure
+{
+    [self authenticateUserWithCredential];
+    [self.OAuth2Manager GET:[NSString stringWithFormat:@"%@/%@",self.baseURL, locationId]
+                 parameters:nil
+                    success:^(AFHTTPRequestOperation *operation, id responseObject)
+     {
+         success(responseObject);
+     }
+                    failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         failure(error);
+     }];
+}
+
+- (void)updateLocationWithLocationId:(NSString *)locationId
+                              NewName:(NSString *)newName
+                              Success:(void (^)(id responseObject))success
+                              Failure:(void (^)(NSError *error))failure
+{
+    [self authenticateUserWithCredential];
+    
+    NSDictionary * parameters = @{
+                                  @"name":newName,
+                                  };
+    
+    [self.OAuth2Manager PUT:[NSString stringWithFormat:@"%@/%@",self.baseURL, locationId]
+                 parameters:parameters
+                    success:^(AFHTTPRequestOperation *operation, id responseObject)
+     {
+         success(responseObject);
+     }
+                    failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         failure(error);
+     }];
+}
+
+
+- (void)deleteLocationWithLocationId:(NSString *)locationId
+                              Success:(void (^)(id responseObject))success
+                              Failure:(void (^)(NSError *error))failure
+{
+    [self authenticateUserWithCredential];
+    [self.OAuth2Manager DELETE:[NSString stringWithFormat:@"%@/%@",self.baseURL, locationId]
+                    parameters:nil
+                       success:^(AFHTTPRequestOperation *operation, id responseObject)
+     {
+         success(responseObject);
+     }
+                       failure:^(AFHTTPRequestOperation *operation, NSError *error)
+     {
+         failure(error);
+     }];
+}
+
 @end
