99e317f3d50ece22c8a805955894a177fc5ecc84
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-19 05:11:59 +0100

    add link to LocationAddView

--- HackatonWatchOs-Aveugle/Views/Location/Home/GMLocationHomeViewController.m
+++ HackatonWatchOs-Aveugle/Views/Location/Home/GMLocationHomeViewController.m
@@ -7,31 +7,22 @@
 //
 
 #import "GMLocationHomeViewController.h"
+#import "GMLocationAddViewController.h"
 
 @interface GMLocationHomeViewController ()
 
+- (IBAction)goToAddView:(UIButton *)sender;
 @end
 
 @implementation GMLocationHomeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)goToAddView:(UIButton *)sender
+{
+    GMLocationAddViewController * locationAddView = [[GMLocationAddViewController alloc] init];
+    [self.navigationController pushViewController:locationAddView animated:YES];
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 @end
