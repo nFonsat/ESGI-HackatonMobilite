3808a673af556429fc4b0a84297029a12d164205
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-19 03:41:34 +0100

    add location add view

---
+++ HackatonWatchOs-Aveugle/Views/Location/Add/GMLocationAddViewController.m
@@ -0,0 +1,37 @@
+//
+//  GMLocationAddViewController.m
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 19/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "GMLocationAddViewController.h"
+
+@interface GMLocationAddViewController ()
+
+@end
+
+@implementation GMLocationAddViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
