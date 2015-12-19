37213f4722c3b0eb02c3bb4f9a8f5dbad24263ae
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-19 11:11:46 +0100

    Add custom UITableViewCelll

---
+++ HackatonWatchOs-Aveugle/Components/GMPlaceTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  GMPlaceTableViewCell.h
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 19/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMPlaceTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView * iconPlace;
+@property (weak, nonatomic) IBOutlet UILabel * labelPlace;
+
+@end
---
+++ HackatonWatchOs-Aveugle/Components/GMPlaceTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  GMPlaceTableViewCell.m
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 19/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "GMPlaceTableViewCell.h"
+
+@implementation GMPlaceTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
