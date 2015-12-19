7b520524c0798f98b09758ee472cfebab1b0ec24
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-19 04:07:10 +0100

    add GMWebLocationAPI and imply header file

---
+++ HackatonWatchOs-Aveugle/Web/API/Location/GMWebLocationAPI.m
@@ -0,0 +1,13 @@
+//
+//  GMWebLocationAPI.m
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 19/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "GMWebLocationAPI.h"
+
+@implementation GMWebLocationAPI
+
+@end
