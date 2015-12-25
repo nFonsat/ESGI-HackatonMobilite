15f3ede75a3984d1f7f96e9e7f6b6fe320a9026a
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2015-12-25 20:05:52 +0100

    add Location Cell

---
+++ HackatonWatchOs-Aveugle/Components/TableViewCell/Location/GMLocationTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  GMLocationTableViewCell.h
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 25/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMLocationTableViewCell : UITableViewCell
+
+@end
---
+++ HackatonWatchOs-Aveugle/Components/TableViewCell/Location/GMLocationTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  GMLocationTableViewCell.m
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 25/12/2015.
+//  Copyright © 2015 Etudiant. All rights reserved.
+//
+
+#import "GMLocationTableViewCell.h"
+
+@implementation GMLocationTableViewCell
+
+- (void)awakeFromNib
+{
+    
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+}
+
+@end
