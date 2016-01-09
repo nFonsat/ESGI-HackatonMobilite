53a456f9e0c6ec0dfe14c403236d166a36187ab0
Nicolas Fonsat <fonsat.nicolas@gmail.com>
2016-01-09 20:48:41 +0100

    add new class UIColor and GMBaseViewController

---
+++ HackatonWatchOs-Aveugle/Components/Color/UIColor+GMColor.h
@@ -0,0 +1,19 @@
+//
+//  UIColor+GMColor.h
+//  HackatonWatchOs-Aveugle
+//
+//  Created by Nicolas Fonsat on 09/01/2016.
+//  Copyright © 2016 Etudiant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (GMColor)
+
++ (UIColor *)favoriteColor;
+
++ (UIColor *)mapColor;
+
++ (UIColor *)randomColor;
+
+@end
